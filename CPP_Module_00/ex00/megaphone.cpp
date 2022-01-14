d91e9ac6b1257bf2b942e6223345baa8c8ade7eb
melperri <melperri@student.42paris.fr>
2022-01-14 14:42:16 +0100

    ex00

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,8 +6,26 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 20:14:56 by melperri          #+#    #+#             */
-/*   Updated: 2022/01/13 20:25:40 by melperri         ###   ########.fr       */
+/*   Updated: 2022/01/14 14:39:55 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
 
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i != argc; i++) {
+			for (int j = 0; argv[i][j]; j++) {
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+		}
+		std::cout << '\n';
+	}
+	return (0);
+}
