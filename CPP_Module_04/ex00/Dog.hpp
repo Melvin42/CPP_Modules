afaf17273aaad34271876c8c60bc70aa05b3c7a7
melperri <melperri@student.42.fr>
2022-03-12 04:33:57 +0100

    delete color define

--- CPP_Module_04/ex00/Dog.hpp
+++ CPP_Module_04/ex00/Dog.hpp
@@ -6,15 +6,12 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 21:54:31 by melperri          #+#    #+#             */
-/*   Updated: 2022/03/11 21:54:31 by melperri         ###   ########.fr       */
+/*   Updated: 2022/03/12 04:33:03 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DOG_HPP
 #define DOG_HPP
-
-#define GREEN_IT "\033[32;3m"
-#define END_COLOR "\033[0m"
 
 #include "Animal.hpp"
 
