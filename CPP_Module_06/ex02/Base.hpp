e0c837887c706c2853e54cac3d5a6d530bada55a
melperri <melperri@student.42.fr>
2022-03-25 15:37:54 +0100

    all good

---
+++ CPP_Module_06/ex02/Base.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 13:23:14 by melperri          #+#    #+#             */
+/*   Updated: 2022/03/25 13:23:14 by melperri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#define GREEN_IT "\033[32;3m"
+#define END_COLOR "\033[0m"
+
+#include <iostream>
+
+class Base {
+	public:
+		virtual	~Base();
+};
+
+#endif
---
+++ CPP_Module_06/ex02/main.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 04:49:25 by melperri          #+#    #+#             */
+/*   Updated: 2022/03/25 15:37:17 by melperri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base	*generate() {
+	srand((unsigned)time(0));
+
+	Base			*base = NULL;
+	unsigned int	randNum = (rand() % 3) + 1;
+
+	switch (randNum) {
+		case 1:
+			base = new A();
+			break ;
+		case 2:
+			base = new B();
+			break ;
+		case 3:
+			base = new C();
+			break ;
+	}
+
+	return base;
+}
+
+void	identify(Base *p) {
+	if (!p)
+		return ;
+
+	A	*a = dynamic_cast<A*>(p);
+	B	*b = dynamic_cast<B*>(p);
+	C	*c = dynamic_cast<C*>(p);
+
+	if (a != NULL)
+		std::cout << "Type class is : A" << std::endl;
+	else if (b != NULL)
+		std::cout << "Type class is : B" << std::endl;
+	else if (c != NULL)
+		std::cout << "Type class is : C" << std::endl;
+	return ;
+}
+
+void	identify(Base &p) {
+	try {
+		A	&a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "Type class is : A" << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		B	&b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "Type class is : B" << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		C	&c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "Type class is : C" << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	return ;
+}
+
+int	main() {
+	Base	*p = generate();
+	Base	&ref = *p;
+
+	identify(p);
+	identify(ref);
+	delete p;
+	return 0;
+}
