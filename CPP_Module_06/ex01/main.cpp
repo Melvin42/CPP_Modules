b4a18c56853db86141c7c4068a57ea1bc01d5a04
melperri <melperri@student.42.fr>
2022-03-25 13:17:31 +0100

    OK

--- CPP_Module_06/ex01/main.cpp
+++ CPP_Module_06/ex01/main.cpp
@@ -6,14 +6,16 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 04:49:25 by melperri          #+#    #+#             */
-/*   Updated: 2022/03/22 05:14:13 by melperri         ###   ########.fr       */
+/*   Updated: 2022/03/25 13:16:43 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdint>
+#include <iostream>
+#include <stdint.h>
+
 
 struct	Data {
-	void	*data;
+	int	*data;
 };
 
 uintptr_t	serialize(Data *str) {
@@ -27,11 +29,20 @@ Data	*deserialize(uintptr_t raw) {
 int	main() {
 
 	Data	obj;
+	int		n = 42;
 	
-	obj.data = (void *)42;
-	serialize();
-	deserialize();
-	
-	std::cout << obj.data << std::endl;
+	obj.data = &n;
+	std::cout << "Data = " << *obj.data << std::endl;
+	std::cout << "Serialize = " << serialize(&obj) << std::endl;
+	std::cout << "Data = " << *obj.data << std::endl;
+	std::cout << "Deserialize = " << deserialize((uintptr_t)obj.data) << std::endl;
+	std::cout << "Data = " << *obj.data << std::endl;
+	n = 48;
+	obj.data = &n;
+	std::cout << "Serialize = " << serialize(&obj) << std::endl;
+	std::cout << "Data = " << *obj.data << std::endl;
+	std::cout << "Deserialize = " << deserialize((uintptr_t)obj.data) << std::endl;
+	std::cout << "Serialize = " << serialize(&obj) << std::endl;
+	std::cout << "Data = " << *obj.data << std::endl;
 	return 0;
 }
