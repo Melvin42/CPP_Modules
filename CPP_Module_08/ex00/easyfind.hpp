f04c2b9dc28de3f6416054c6cbb560edbaeac293
melperri <melperri@student.42.fr>
2022-04-03 12:00:06 +0200

    ex00 ok

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -6,23 +6,31 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:04:51 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/01 17:39:15 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/03 11:59:23 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <vector>
 #include <algorithm>
+#include <exception>
 
-template <typename T> T &easyfind(T t, int n) {
-
-	for (size_t	i = 0; i < 4; i++) {
-		if (t[i] == n)
-			return t[i];
+class NoValueFoundException : public std::exception {
+	public :
+		virtual const char *what() const throw() {
+			return "Don't found match value";
 	}
-	return t;
+};
+
+template <typename T> typename T::const_iterator easyfind(T &t, int n) {
+	typename T::const_iterator	iter;
+
+	iter = std::find(t.begin(), t.end(), n);
+	if (iter == t.end()) {
+		throw NoValueFoundException();
+	}
+	return iter;
 };
 
 #endif
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -6,17 +6,41 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:03:10 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/01 17:35:52 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/03 11:57:10 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
+#include <list>
 #include "easyfind.hpp"
 
 int	main() {
+	try {
+		std::vector<int>					tab(4, 10);
+		std::vector<int>::const_iterator	iter;
 
-	int	tab[4] = {1, 2, 3, 4};
+		iter = ::easyfind(tab, 10);
+		std::cout << *iter << std::endl;
+		iter = ::easyfind(tab, 1);
+		std::cout << *iter << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::list<int>						lst;
+		std::list<int>::const_iterator		iter;
 
-	::easyfind(tab, 4);
+		lst.push_back(10);
+		lst.push_back(11);
+		lst.push_back(12);
+		lst.push_back(12);
+		lst.push_back(42);
+		lst.push_back(15);
+		iter = ::easyfind(lst, 42);
+		std::cout << *iter << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
