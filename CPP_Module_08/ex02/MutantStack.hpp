fb66448b9af63d3f4b761c1a69d0a1432377adc4
melperri <melperri@student.42.fr>
2022-04-03 22:08:40 +0200

    save

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -6,23 +6,58 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:24:00 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/01 15:24:00 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/03 21:51:27 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack {
+#include <iostream>
+#include <stack>
+
+template <class T> class MutantStack : public std::stack<T> {
 	public:
 
 		MutantStack();
 		MutantStack(const MutantStack &mutantstack);
 		~MutantStack();
 
+//		empty();
+//		void	push();
+//		int		size();
+//		emplace();
+//		top();
+//		swap;
+
 		MutantStack &operator=(const MutantStack &mutantstack);
 
 	private:
 };
+
+template <class T>
+MutantStack<T>::MutantStack() {
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+template <class T>
+MutantStack<T>::MutantStack(const MutantStack &mutantstack) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = mutantstack;
+	return ;
+}
+
+template <class T>
+MutantStack<T>::~MutantStack() {
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+template <class T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &mutantstack) {
+	std::cout << "Copy assignement operator called" << std::endl;
+	return *this;
+}
 
 #endif
--- CPP_Module_08/ex02/main.cpp
+++ CPP_Module_08/ex02/main.cpp
@@ -6,12 +6,35 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:09:05 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/01 15:10:14 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/03 21:32:53 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 int	main() {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+//	MutantStack<int>::iterator it = mstack.begin();
+//	MutantStack<int>::iterator ite = mstack.end();
+//	++it;
+//	--it;
+//	while (it != ite)
+//	{
+//		std::cout << *it << std::endl;
+//		++it;
+//	}
+//	std::stack<int> s(mstack);
 	return 0;
 }
