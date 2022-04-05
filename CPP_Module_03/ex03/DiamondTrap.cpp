02438ed0b73d3b519cfa7f9d57a506fcb2f57be3
melperri <melperri@student.42paris.fr>
2022-04-05 12:35:06 +0200

    cpp03 OK

--- CPP_Module_03/ex03/DiamondTrap.cpp
+++ CPP_Module_03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 19:14:48 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/04 11:49:31 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/05 12:17:28 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,18 @@
 
 DiamondTrap::DiamondTrap() : ScavTrap("Diamond"), FragTrap("Diamond") {
 	ScavTrap	scavtrap;
-	FragTrap	fragtrap;
 
-	this->setHit(fragtrap.getHit());
 	this->setEnergy(scavtrap.getEnergy());
-	this->setAttack(fragtrap.getAttack());
-	std::cout << GREEN_IT << "Diamondtrap " << this->ScavTrap::getName()
+	std::cout << GREEN_IT << "Diamondtrap " << this->getName()
 		<< " default constructor called" << END_COLOR << std::endl;
 	return ;
 }
 
 DiamondTrap::DiamondTrap(std::string name) : ScavTrap(name), FragTrap(name) {
 	ScavTrap	scavtrap;
-	FragTrap	fragtrap;
 
-	this->setHit(fragtrap.getHit());
 	this->setEnergy(scavtrap.getEnergy());
-	this->setAttack(fragtrap.getAttack());
-	std::cout << GREEN_IT << "Diamondtrap " << this->ScavTrap::getName()
+	std::cout << GREEN_IT << "Diamondtrap " << this->getName()
 		<< " parametric constructor called" << END_COLOR << std::endl;
 	return ;
 }
@@ -39,30 +33,30 @@ DiamondTrap::DiamondTrap(std::string name) : ScavTrap(name), FragTrap(name) {
 DiamondTrap::DiamondTrap(const DiamondTrap &diamondtrap)
 	: ClapTrap(diamondtrap) , ScavTrap(diamondtrap), FragTrap(diamondtrap) {
 	*this = diamondtrap;
-	std::cout << GREEN_IT << "Diamondtrap " << this->ScavTrap::getName()
+	std::cout << GREEN_IT << "Diamondtrap " << this->getName()
 		<< " copy constructor called" << END_COLOR << std::endl;
 	return ;
 }
 
 DiamondTrap::~DiamondTrap() {
-	std::cout << GREEN_IT << "Diamondtrap " << this->ScavTrap::getName()
+	std::cout << GREEN_IT << "Diamondtrap " << this->getName()
 		<< " destructor called" << END_COLOR << std::endl;
 	return ;
 }
 
 DiamondTrap &DiamondTrap::operator=(const DiamondTrap &diamondtrap) {
-	this->ScavTrap::setName(diamondtrap.ScavTrap::getName());
-	this->FragTrap::setHit(diamondtrap.FragTrap::getHit());
-	this->ScavTrap::setEnergy(diamondtrap.ScavTrap::getEnergy());
-	this->FragTrap::setAttack(diamondtrap.FragTrap::getAttack());
-	std::cout << GREEN_IT << "Diamondtrap " << this->ScavTrap::getName()
+	this->setName(diamondtrap.getName());
+	this->setHit(diamondtrap.getHit());
+	this->setEnergy(diamondtrap.getEnergy());
+	this->setAttack(diamondtrap.getAttack());
+	std::cout << GREEN_IT << "Diamondtrap " << this->getName()
 		<< " copy assignement operator called" << END_COLOR << std::endl;
 	return *this;
 }
 
 void	DiamondTrap::whoAmI(void) {
-	if (this->FragTrap::getHit() > 0)
-		std::cout << this->ScavTrap::getName() << " is now in Gate keeper mode" << std::endl;
+	if (this->getHit() > 0)
+		std::cout << this->getName() << " is now in Gate keeper mode" << std::endl;
 	else
-		std::cout << this->ScavTrap::getName() << " can't go in Gate keeper mode" << std::endl;
+		std::cout << this->getName() << " can't go in Gate keeper mode" << std::endl;
 }
