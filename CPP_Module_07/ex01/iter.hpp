437d5a391af760216a393a3752406fddcc783ec7
melperri <melperri@student.42paris.fr>
2022-04-12 14:18:28 +0200

    better iter of 42 :P

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -6,15 +6,20 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 15:05:44 by melperri          #+#    #+#             */
-/*   Updated: 2022/03/31 17:52:42 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/12 14:17:17 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
+template <typename T, typename R, typename F> void	iter(T *addr, size_t length, R (*fct)(F)) {
+	for (size_t i = 0; i < length; i++) {
+		fct(addr[i]);
+	}
+}
 
-template <typename T, typename F> void	iter(T &addr, size_t length, F fct) {
+template <typename T> void	iter(T *addr, size_t length, void (*fct)(T const &)) {
 	for (size_t i = 0; i < length; i++) {
 		fct(addr[i]);
 	}
--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 15:03:23 by melperri          #+#    #+#             */
-/*   Updated: 2022/03/31 18:02:19 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/12 14:16:09 by melperri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,48 @@ void	ft_display_char(char i) {
 	std::cout << i << '\n';
 }
 
+class Awesome {
+	public:
+		Awesome(void):_n(42) { return;}
+		int get(void) const {return this->_n;}
+	private:
+		int	_n;
+};
+
+std::ostream &operator<<(std::ostream &o, Awesome const & rhs ) {
+	o << rhs.get();
+	return o;
+}
+
+template <typename T>
+void	print(T const &x) {
+	std::cout << x << std::endl;
+	return ;
+}
+
 int main() {
-	int	*tab = new int(4);
+	{
+		int	*tab = new int[4];
+	
+		tab[0] = 0;
+		tab[1] = 1;
+		tab[2] = 2;
+		tab[3] = 3;
+	
+		::iter(tab, 4, ft_display_int);
+	
+		char	str[] = {"coucou"};
+	
+		::iter(str, 7, ft_display_char);
+	
+		delete [] tab;
+	}
+	{
+		int tab[] = {0, 1, 2, 3, 4};
+		Awesome tab2[5];
 
-	tab[0] = 0;
-	tab[1] = 1;
-	tab[2] = 2;
-	tab[3] = 3;
-
-	::iter(tab, 4, ft_display_int);
-
-	char	str[] = {"coucou"};
-
-	::iter(str, 7, ft_display_char);
-
-	delete [] tab;
+		iter(tab, 5, print);
+		iter(tab2, 5, print);
+	}
 	return 0;
 }
